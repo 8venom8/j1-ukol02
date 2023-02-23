0d5908c1bcadc12c2f4efeb8f40fa6ae8e1115ae
Vendula Stravová <venom111@email.cz>
2023-02-23 10:52:39 +0100

    vytvoreni trojuhelniku, ctverce..

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,6 +9,44 @@ public class HlavniProgram {
 
     }
 
+    private void drawRectangle() {
+        for (int i = 0; i < 2; i++) {
+            zofka.penDown();
+            zofka.move(50);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+    }
+
+    private void drawSquare() {
+        for (int i = 0; i < 4; i++) {
+            zofka.penDown();
+            zofka.move(80);
+            zofka.turnRight(90);
+        }
+    }
+
+    private void drawCircle() {
+        for (int i = 0; i < 72; i++) {
+            zofka.penDown();
+            zofka.turnRight(5);
+            zofka.move(5);
+        }
+    }
+
+    private void drawTriangle() {
+        zofka.penDown();
+        zofka.turnRight(90);
+        zofka.turnLeft(60);
+        zofka.move(80);
+        zofka.turnRight(120);
+        zofka.move(80);
+        zofka.turnRight(120);
+        zofka.move(80);
+        zofka.turnRight(90);
+    }
+
     public static void main(String[] args) {
         new HlavniProgram().start();
     }
